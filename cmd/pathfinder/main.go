f89c837677a5b3bbc59475c340e4b2d61b41a90d
Jack <109554692+SamuraiJack96@users.noreply.github.com>
2026-01-07 13:04:22 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/pathfinder/main.go
@@ -0,0 +1,23 @@
+// cmd/pathfinder/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "pathfinder/internal/pathfinder"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := pathfinder.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
